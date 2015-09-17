e01f6ef90c9e76cc11eedc2b62f3450dfc1d73cd
crackhopper <710591209@qq.com>
2015-09-18 01:08:39 +0800

    增加了mocha的grunt任务。写了简单的示例。调通了一些问题。
    TODO：
    1. jshint, watch都集成进来。
    2. 单元测试覆盖一下。
    3. 以这个为基础，书写grunt-init-ligle-model项目。

---
+++ test/member_test.js
@@ -0,0 +1,48 @@
+var expect = require('chai').expect;
+var should = require('chai').should();
+var ligle = require('ligle-engine')({
+  loggerLevel:"INFO",
+  db:{
+    name:'ligle-test',
+    host: '127.0.0.1',
+    port:27017
+  }
+});
+
+
+/// 下面两个变量，辅助测试延迟相应。
+var delay = 100;// test must be delayed to avoid conflict
+var n_test = 1; // used for delay
+
+
+require('../lib/member.js')(ligle);
+describe('ligle-model',function(){
+  var Model = ligle.model.Member;
+  before(function(done){
+    ligle.db.start(function(err,db){
+      done();
+    });
+  });
+
+  it('new object',function(){
+    var m = new Model();
+    should.exist(m.emailInfo);
+    should.exist(m.cellInfo);
+  });
+
+  it('saving',function(done){
+    var m = new Model({name:'123'});
+    m.save(function(err,obj){
+      should.not.exist(err);
+      expect(obj.name).equal('123');
+      done();
+    });
+  });
+
+  after(function(done){
+    ligle.db.dropDatabase(function(){
+      ligle.db.close();
+      done();
+    });
+  });
+});
